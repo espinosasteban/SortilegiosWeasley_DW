b1b78db525a870d979e9fb70cbb5f83f0007b87b
espinosasteban <esespinosap@unal.edu.co>
2025-01-18 15:05:22 -0500

    Renombre de carpetas
    Se cambiaron los nombres a todos los archivos y carpetas a español

---
+++ SortilegiosWeasley/components/productoSeccion.tsx
@@ -0,0 +1,22 @@
+import '../styles/productoSeccion.css';
+import {Articulo} from "../tipos.tsx";
+
+interface ProductoSeccionProps {
+    articulo: Articulo;
+}
+
+function ProductoSeccion({ articulo }: ProductoSeccionProps) {
+
+    const { nombre, precio, descripcion, seccion, imagen } = articulo;
+
+    return (
+        <div className="product">
+            <img src={imagen} alt={nombre} className="product-image" />
+            <h1 className="product-title">{nombre}</h1>
+            <p className="product-description">{descripcion}</p>
+            <p className="product-price">${precio}</p>
+        </div>
+    );
+}
+
+export default ProductoSeccion;
